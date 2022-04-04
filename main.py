4ae4d4a095c80df766425151ca9a2bf7ff848653
Gjum <code.gjum@gmail.com>
2022-04-04 06:38:06 +0200

    user agent

--- main.py
+++ main.py
@@ -472,7 +472,7 @@ class PlaceClient:
                             client.proxies = proxy.get_random_proxy(self)
                             client.headers.update(
                                 {
-                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+                                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
                                 }
                             )
 
